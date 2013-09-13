b3c8582c9d7e06f68428e4803c204fe06b9fb618
Jeff Perry <jeffsp@gmail.com>
2013-09-13 11:01:54 -0500

    add documentation m-file

---
+++ matleap.m
@@ -0,0 +1,19 @@
+% matleap Leap Motion Controller Interface
+% ----------------------------------------
+%
+% Copyright (C) 2013
+% Center for Perceptual Systems
+% University of Texas at Austin
+%
+% FUNCTIONS
+% ---------
+% v=matleap(0)      Get the version number
+% f=matleap(1)      Get a frame from the leap motion controller
+%
+% HISTORY
+% -------
+% jsp Fri Sep 13 10:47:36 CDT 2013 Created
+%
+% SEE ALSO
+% --------
+% https://www.leapmotion.com/
