4699c5e596f04f8a210bcd16d04d8e5b62e28ae2
Jeff Perry <jeffsp@gmail.com>
2013-09-13 10:40:24 -0500

    add back code for printing debug information

--- matleap.h
+++ matleap.h
@@ -16,6 +16,7 @@
 namespace matleap
 {
 
+/// @brief a leap frame
 struct frame
 {
     int64_t id;
@@ -23,29 +24,48 @@ struct frame
     Leap::PointableList pointables;
 };
 
+/// @brief leap frame grabber interface
 class frame_grabber
 {
     private:
+    bool debug;
     Leap::Controller controller;
     frame current_frame;
     public:
+    /// @brief constructor
     frame_grabber ()
+        : debug (false)
     {
-        mexPrintf ("Initializing matleap frame grabber version %d.%d\n", MAJOR_REVISION, MINOR_REVISION);
         // receive frames even when you don't have focus
         controller.setPolicyFlags (Leap::Controller::POLICY_BACKGROUND_FRAMES);
     }
+    /// @brief destructor
     ~frame_grabber ()
     {
-        mexPrintf ("Closing matleap frame grabber\n");
+        if (debug)
+            mexPrintf ("Closing matleap frame grabber\n");
     }
+    /// @brief debug member access
+    ///
+    /// @param flag turn it on/off
+    void set_debug (bool flag)
+    {
+        if (flag)
+            mexPrintf ("Setting debug on\n");
+        debug = flag;
+    }
+    /// @brief get a frame from the controller
+    ///
+    /// @return the frame
     const frame &get_frame ()
     {
         const Leap::Frame &f = controller.frame ();
         current_frame.id = f.id ();
-        mexPrintf ("Got frame %d\n", current_frame.id);
+        if (debug)
+            mexPrintf ("Got frame with id %d\n", current_frame.id);
         current_frame.timestamp = f.timestamp ();
         current_frame.pointables = f.pointables ();
+        return current_frame;
     }
 };
 
