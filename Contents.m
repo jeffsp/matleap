bec8ddf08a081ac6008db6e998661511e6716b41
Jeff Perry <jeffsp@gmail.com>
2013-11-20 11:00:08 -0600

    add contents doc

---
+++ Contents.m
@@ -0,0 +1,18 @@
+% matleap Leap Motion Controller Interface
+% ----------------------------------------
+%
+% Copyright (C) 2013 Jeff Perry
+%
+% FUNCTIONS
+% ---------
+% v=matleap_version  Get the version number
+% f=matleap_frame    Get a frame from the leap motion controller
+%
+% HISTORY
+% -------
+% jsp Wed Nov 20 10:36:45 CST 2013 Created
+%
+% SEE ALSO
+% --------
+% https://www.leapmotion.com/
+% https://developer.leapmotion.com/
